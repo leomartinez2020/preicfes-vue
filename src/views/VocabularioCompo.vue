a9fafb2e0ff32c57b597812d8e51f3596335ddd9
root <root@LAPTOP-5HJG55H7.localdomain>
2023-06-18 11:17:18 -0500

    Simplify json data

--- src/views/VocabularioCompo.vue
+++ src/views/VocabularioCompo.vue
@@ -24,7 +24,7 @@ export default {
     },
     procesar() {
       for (let key in this.responses) {
-         const coco = this.quiz.secciones[0].preguntas.filter(obj => obj.pk === parseInt(key));
+         const coco = this.quiz.preguntas.filter(obj => obj.pk === parseInt(key));
          if (coco[0].letra === this.responses[key]) {
            this.correctResponses++;
            coco[0].correct = true;
@@ -44,23 +44,21 @@ export default {
 
 <div class="w-4/5 mx-auto pt-8">
 <!-- <form @submit.prevent="submit"> -->
-<form v-on:submit.prevent>
-<!-- Seccion tipo 1 -->
-<div v-for="seccion in quiz.secciones">
-  <div v-if="seccion.tipo === 'tipouno'">
-    <p class="font-bold text-xl text-yellow-700 text-center">{{ seccion.titulo }}</p>
-    <p class="font-bold text-xl text-yellow-700 text-center">{{ seccion.texto }}</p>
-    <div v-if="seccion.descripcion">{{ seccion.descripcion }}</div>
-    <p class="font-bold text-2xl text-yellow-700 text-center">{{ seccion.tema }}</p>
+  <form v-on:submit.prevent>
+  <!-- Seccion tipo 1 -->
+  <div v-if="quiz.tipo === 'tipouno'">
+    <p class="font-bold text-xl text-yellow-700 text-center">{{ quiz.texto }}</p>
+    <div v-if="quiz.descripcion">{{ quiz.descripcion }}</div>
+    <p class="font-bold text-2xl text-yellow-700 text-center">{{ quiz.tema }}</p>
     <p><span class="px-2 bg-blue-300 font-bold text-yellow-700 rounded-md">Ejemplo</span></p>
-    <p><span class="font-bold">0.</span> {{ seccion.ejemplo.pregunta }}</p>
+    <p><span class="font-bold">0.</span> {{ quiz.ejemplo.pregunta }}</p>
     <!-- Cuadro de opciones para el ejemplo -->
     <CuadroOpciones />
     <section class="my-10 grid grid-cols-2 md:grid-cols-3 gap-4">
       <div class="sm:border-r-4 border-yellow-700 col-span-2">
         <h3 class="text-xl text-amber-700 font-bold border-b-4 border-yellow-700 mr-4">Descripciones</h3>
           <!-- Cuadro de opciones -->
-          <div class="pb-4" v-for="(pregunta, index) in seccion.preguntas">
+          <div class="pb-4" v-for="(pregunta, index) in quiz.preguntas">
             <p class="">
               <span class="font-bold">{{  index + 1 }}.</span>
               {{ pregunta.texto }}
@@ -74,7 +72,7 @@ export default {
       </div>
       <div class="">
         <h3 class="text-xl text-amber-700 font-bold border-b-4 border-yellow-700">Palabras</h3>
-        <div v-for="(value, key, index) in seccion.palabras" :key="index">
+        <div v-for="(value, key, index) in quiz.palabras" :key="index">
           <span v-if="key === 'H'" class="mt-2 p-1 inline-block text-lg font-bold text-yellow-700 rounded-md border-blue-300 bg-blue-300"><span class="font-bold">{{ key }}.</span> {{value}}</span>
           <p v-else class="text-lg py-3"><span class="font-bold">{{ key }}.</span> {{value}}</p>
         </div>
@@ -82,15 +80,13 @@ export default {
     </section>
     <p class="text-xl text-blue-700 text-center"># respuestas correctas: {{ correctResponses }}</p>
   </div>
-<!-- Fin seccion tipo 1 -->
+  <!-- Fin seccion tipo 1 -->
 
-<!-- ENFOR PRINCIPAL OJOOOOOOO -->
-</div>
-   <!-- Botón enviar -->
-   <div class="text-center my-6">
+    <!-- Botón enviar -->
+    <div class="text-center my-6">
      <button @click="procesar" class="cursor-pointer mt-4 text-white bg-slate-600 hover:bg-slate-800 text-lg font-medium rounded-lg px-5 focus:ring-4 focus:ring-blue-300 px-4">Evaluar</button>
-   </div>
-</form>
+    </div>
+  </form>
 </div>
 </template>
 
